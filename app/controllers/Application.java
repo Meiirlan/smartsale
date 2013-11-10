1204cd18bb8aa6f743d211928c2d9e1566980cc9
sulta <zh.sulta@gmail.com>
2013-11-10 18:02:25 +0600

    login and signup started

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 import models.*;
 
@@ -12,5 +15,15 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
-
+    public static void signup() {    	
+    	render();
+    }
+    public static void save(Long id,
+    		@Required(message="Email is required") String email, 
+    		@Required(message="Password is required") String pwd, 
+    		@Required(message="Password confirmation is required") String cpwd) throws Throwable {
+    	
+    	  	
+    	Secure.login();    	
+    }
 }
